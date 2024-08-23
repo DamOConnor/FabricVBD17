85aa590dd19d57385cef801b67b4021847f7bdab
DamOConnor <111589807+DamOConnor@users.noreply.github.com>
2024-08-23 19:32:10 +0100

    Committing 6 items from workspace 42c69ce5-79b8-45e4-93ac-e3933ffa643d

---
+++ user06/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "05cb85d9-607e-4f5f-ae4a-88bdc76d590f",
+# META       "default_lakehouse_name": "lh_tpch06",
+# META       "default_lakehouse_workspace_id": "42c69ce5-79b8-45e4-93ac-e3933ffa643d",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "05cb85d9-607e-4f5f-ae4a-88bdc76d590f"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
